1033148402ec5cdb524651bd62f21e7720ffde10
bilal-boop <67937279+bilal-boop@users.noreply.github.com>
2023-09-01 02:01:41 +0500

    Loading Screen Added

--- js/loading.js
+++ js/loading.js
@@ -1,27 +1,7 @@
-gsap.fromTo(
-  ".loading-page",
-  { opacity: 1 },
-  {
-    opacity: 0,
-    display: "none",
-    duration: 1.5,
-    delay: 3.5,
-  }
-);
-
-gsap.fromTo(
-  ".logo-name-loading",
-  {
-    y: 50,
-    opacity: 0,
-  },
-  {
-    y: 0,
-    opacity: 1,
-    duration: 2,
-    delay: 0.5,
-  }
-);
-gsap.fromTo(
-  ""
-)
+window.onload = function () {
+  var loadingPage = document.querySelector('.loading-page');
+  loadingPage.style.opacity = 0; // Set opacity to fade out
+  setTimeout(function () {
+    loadingPage.style.display = 'none'; // Hide the loading screen after fading
+  }, 1000); // Adjust the delay based on your preference
+};
